afc339b31347772cfe89b8825dcfe957c0a47330
Rafaël Carré <funman@videolan.org>
2016-03-08 13:43:41 +0100

    Build PCI subsystem in x86_64
    I've no idea why the X86_32 limitation is here or whether it's needed or not

--- arch/x86/kernel/mmconf-fam10h_64.c
+++ arch/x86/kernel/mmconf-fam10h_64.c
@@ -184,7 +184,7 @@ void __cpuinit fam10h_check_enable_mmcfg(void)
 			FAM10H_MMIO_CONF_BUSRANGE_MASK;
 
 		/* only trust the one handle 256 buses, if acpi=off */
-		if (!acpi_pci_disabled || busnbits >= 8) {
+		if (/*!acpi_pci_disabled*/ 0 || busnbits >= 8) {
 			u64 base = val & MMCONF_MASK;
 
 			if (!fam10h_pci_mmconf_base) {
